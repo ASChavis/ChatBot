e1e1ad491664a30ae8b6caa5682633a74f3ade17
ASChavis <126298472+ASChavis@users.noreply.github.com>
2024-07-12 22:36:47 -0400

    Working on CSS and JS

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ var sendBtn = document.getElementById('sendBtn');
 var textbox = document.getElementById('userInquiry');
 var chatContainer = document.getElementById('chatContainer');
 
-chatBotSendMessage("Hi from ChatBot");
 
 function chatBotSendMessage(messageText){
     var messageElement = document.createElement('div');
@@ -13,6 +12,7 @@ function chatBotSendMessage(messageText){
     messageElement.classList.add('shadow-sm');
     messageElement.style.margin='10px';
     messageElement.style.padding='5px';
+    messageElement.style.backgroundColor='rgb(219, 180, 121)';
 
     messageElement.innerHTML = '<span>ChatBot: </span>'+
     '<span style='+'margin-top:10px; padding:10px'+'>'+ messageText +'</span>';
@@ -28,17 +28,24 @@ function sendMessage(messageText){
     messageElement.classList.add('shadow-sm');
     messageElement.style.margin='10px';
     messageElement.style.padding='5px';
+    messageElement.style.backgroundColor='rgb(228, 137, 17)';
 
     messageElement.innerHTML = '<span>You: </span>'+
     '<span style='+'margin-top:10px; padding:10px'+'>'+ messageText +'</span>';
+
+messageElement.animate([{easing:'ease-in',opacity:0.4},{opacity:1}],{duration:1000});
 
     chatContainer.appendChild(messageElement);
 }
 
 
 sendBtn.addEventListener('click', function(e){
+    if(textbox.value== ""){
+        alert('Please type in message');
+    }else{
 
     let messageText = textbox.value;
     sendMessage(messageText);
-
+    textbox.value='';
+    }
     });
